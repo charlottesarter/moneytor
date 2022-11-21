28b5659db9f1bf399673b5eee6dc0a4859555fd7
Max Eberlein <m.eberlein01@gmail.com>
2022-11-21 15:27:42 +0900

    added create() function

--- Finance.py
+++ Finance.py
@@ -12,6 +12,12 @@ class Finance:
     def __str__(self):
         return self.amount # TODO: Nice display of the object
         
+    def create():
+        amount_new = float(input('Please input the amount: '))
+        currency_new = str(input('please input the currency: '))
 
+        new_finance = Finance(amount_new, currency_new, '0', '0', '0', True, 20221121)
+        if(new_finance.isinstance()):
+            print('New Object has been created')
 
-    
+    def add(new_finance):
