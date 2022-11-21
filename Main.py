06e7a5ead4c1b86306fd5863ef5a3123e29729c3
Max Eberlein <m.eberlein01@gmail.com>
2022-11-21 15:29:14 +0900

    created read() function

--- Main.py
+++ Main.py
@@ -1,21 +1,22 @@
 from Finance import Finance
 
-f = open('data/sample_file.csv', 'r')
-
 while True:
+    Finance.create()
 
-    line = f.readline() # Read a line sequentially
-    values = line.split(',')
+def read():
+    f = open('data/sample_file.csv', 'r')
+    while True:
+        line = f.readline() # Read a line sequentially
+        values = line.split(',')
 
-    if values[0] == '':
-        print('End of the data')
-    else:
-        finance = Finance(values[0], values[1], values[2], values[3], values[4], values[5], values[6])
-        print(finance)
+        if values[0] == '':
+            print('End of the data')
+        else:
+            finance = Finance(values[0], values[1], values[2], values[3], values[4], values[5], values[6])
+            print(finance)
 
-    if not line:
-        break
+        if not line:
+            break
+        # print(line)
+    f.close()
 
-    # print(line)
-
-f.close()
