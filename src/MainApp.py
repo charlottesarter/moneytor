bc25c3a8518e44a6aa6c4d248f73c3f1a10d8a40
charlottesarter <sarter.charlotte@gmail.com>
2022-12-03 17:16:57 +0100

    The login and register are working

--- src/MainApp.py
+++ src/MainApp.py
@@ -4,56 +4,19 @@ from models.ModelMoneytor import ModelMoneytor
 from models.Transaction import Transaction
 from views.WelcomeView import WelcomeView
 
-
-# class App(tk.Tk):
-#     def __init__(self):
-#         super().__init__()
-
-#         self.title('Moneytor')
-
-#         # create a model TODO
-#         path_data = 'data/sample_file.csv'
-#         model = Transaction.initialize(path_data)
-
-
-#         # create a view and place it on the root window
-#         view = WelcomeView(self)
-#         view.grid(row=0, column=0, padx=10, pady=10)
-
-#         # create a controller
-#         controller = RegisterController(model, view)
-
-#         # set the controller to view
-#         view.set_controller(controller)
-
-
 if __name__ == '__main__':
 
     # Initialization of the model
-
     model = ModelMoneytor()
 
-    model.addUser('toto,bla,2')
-
-    users = model.getAllUsers()
-    for user in users:
-        print(user)
-
     # Create root window of the app
-
     root = tk.Tk()
-    root.geometry('320x200')
+    root.geometry('800x300')
     root.title('Moneytor')
 
-
     # Show the first view : the welcome page
-
     WelcomeView.showWelcomeView(root)
 
-    # welcomeView.showView() --> mettre le code dans showView()
-    
-    # app = App()
-    # app.mainloop()
+    # Run the gui
     root.mainloop()
     
-#TODO how do we handle, if the whole page changes, for example for signup when we click next after each step?
--- src/views/WelcomeView.py
+++ src/views/WelcomeView.py
@@ -12,115 +12,90 @@ from ModelMoneytor import ModelMoneytor
 from views.HomePageView import HomePageView
 
 # If the user click on the 'register' button
-class RegisterWindow(Toplevel):
+class RegisterView():
     
-    def __init__(self, master = None):
-        
-        super().__init__(master = master)
-        self.title("Register")
-        self.geometry("320x200")
-        label = tk.Label(self, text ="Welcome to Moneytor")
-        label.grid(column=0, row=0, sticky=tk.EW, columnspan=2, padx=5, pady=5)
+    def __init__(self):
+
+        self.username = ''
+        self.password = ''
+        self.prefered_currency = 0
+    
+    def showRegisterView(self, root):
+
+        def register_button_clicked():
+
+            # Get username, password and currency
+
+            self.username = getUsername()
+            self.password = getPassword()
+            self.prefered_currency = getCurrency()
+
+            model = ModelMoneytor()
+
+            # We add a new user
+
+            model.addUser(str(self.username) + ',' + str(self.password) + ',' + str(self.prefered_currency))
+            model.user_logged = self.username
+
+            # Destroy register frame
+            frame_register.destroy()
+
+            # Create homepage frame
+            homepage_view = HomePageView()
+            HomePageView.showHomePageView(homepage_view, root)
+
+        def getUsername():
+            return username_entry.get()
+
+        def getPassword():
+            return password_entry.get()
+
+        def getCurrency():
+            return currency_var.get()
+
+        frame_register = tk.Frame(root)
 
         # create widgets
         # username
-        label_username = ttk.Label(self, text='Username:')
+        label_username = ttk.Label(frame_register, text='Username:')
         label_username.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
 
         # username entry
-        self.username_var = tk.StringVar()
-        self.username_entry = ttk.Entry(self, textvariable=self.username_var, width=30)
-        self.username_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+        username_var = tk.StringVar()
+        username_entry = ttk.Entry(frame_register, textvariable=username_var, width=30)
+        username_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
 
         # password
-        label_password = ttk.Label(self, text='Password:')
+        label_password = ttk.Label(frame_register, text='Password:')
         label_password.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
 
         # password entry
-        self.password_var = tk.StringVar()
-        self.password_entry = ttk.Entry(self, textvariable=self.password_var, width=30)
-        self.password_entry.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
+        password_var = tk.StringVar()
+        password_entry = ttk.Entry(frame_register, textvariable=password_var, width=30)
+        password_entry.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
 
         # currency
-        self.label = ttk.Label(self, text='Prefered currency:')
-        self.label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+        label = ttk.Label(frame_register, text='Prefered currency:')
+        label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
 
         # currency entry
-        self.currency_var = tk.IntVar()
-        self.eur = ttk.Radiobutton(self, text='EUR', variable=self.currency_var, value=1) # Currency['EUR'].value
-        self.krw = ttk.Radiobutton(self, text='KRW', variable=self.currency_var, value=2)
-        self.usd = ttk.Radiobutton(self, text='USD', variable=self.currency_var, value=3)
-        self.eur.grid(row=3, column=1, sticky=tk.W, padx=5, pady=5)
-        self.krw.grid(row=3, column=1, sticky=tk.N, padx=5, pady=5)
-        self.usd.grid(row=3, column=1, sticky=tk.E, padx=5, pady=5)
+        currency_var = tk.IntVar()
+        eur = ttk.Radiobutton(frame_register, text='EUR', variable=currency_var, value=1) # Currency['EUR'].value
+        krw = ttk.Radiobutton(frame_register, text='KRW', variable=currency_var, value=2)
+        usd = ttk.Radiobutton(frame_register, text='USD', variable=currency_var, value=3)
+        eur.grid(row=3, column=1, sticky=tk.W, padx=5, pady=5)
+        krw.grid(row=3, column=1, sticky=tk.N, padx=5, pady=5)
+        usd.grid(row=3, column=1, sticky=tk.E, padx=5, pady=5)
 
         # register button
-        self.register_button = ttk.Button(self, text='Register', command=self.register_button_clicked)
-        self.register_button.grid(column=0, row=5, sticky=tk.S, columnspan=2, padx=5, pady=5)
+        register_button = ttk.Button(frame_register, text='Register', command=register_button_clicked)
+        register_button.grid(column=0, row=5, sticky=tk.S, columnspan=2, padx=5, pady=5)
 
         # message
-        self.message_label = ttk.Label(self, text='', foreground='red')
-        self.message_label.grid(column=0, row=4, sticky=tk.EW, columnspan=2, padx=5, pady=5)
+        # message_label = ttk.Label(frame_register, text='', foreground='red')
+        # message_label.grid(column=0, row=4, sticky=tk.EW, columnspan=2, padx=5, pady=5)
 
-        # set the controller
-        # set_controller(RegisterController(User('', '', '')))
-        self.controller = None
-
-    def set_controller(self, controller):
-        """
-        Set the controller
-        :param controller:
-        :return:
-        """
-        self.controller = controller
-
-    def register_button_clicked(self):
-        """
-        Handle button click event
-        :return:
-        """
-        print('coucou')
-        if self.controller:
-            print('ola')
-            self.controller.save(self.username_var.get(), self.password_var.get(), self.currency_var.get())
-
-    def show_error(self, message):
-        """
-        Show an error message
-        :param message:
-        :return:
-        """
-        self.message_label['text'] = message
-        self.message_label['foreground'] = 'red'
-        self.message_label.after(3000, self.hide_message)
-        self.username_entry['foreground'] = 'red'
-
-    def show_success(self, message):
-        """
-        Show a success message
-        :param message:
-        :return:
-        """
-        self.message_label['text'] = message
-        self.message_label['foreground'] = 'green'
-        self.message_label.after(3000, self.hide_message)
-
-        # reset the form
-        self.username_entry['foreground'] = 'black'
-        self.username_var.set('')
-
-        self.password_entry['foreground'] = 'black'
-        self.password_var.set('')
-
-        # self.currency_entry['foreground'] = 'black'
-        self.currency_var.set('')
-
-    def hide_message(self):
-        """
-        Hide the message
-        :return:
-        """
-        self.message_label['text'] = ''
+        frame_register.pack()
 
 class LoginView():
 
@@ -164,7 +139,7 @@ class LoginView():
 
         frame_login = tk.Frame(root)
 
-        label_welcome = tk.Label(frame_login, text='To see your transactions, please log in first')
+        label_welcome = ttk.Label(frame_login, text='To see your transactions, please log in first')
         label_welcome.grid(column=0, row=0, sticky=tk.EW, columnspan=2, padx=5, pady=5)
 
         # username
@@ -175,7 +150,6 @@ class LoginView():
         username = tk.StringVar()
         username_entry = ttk.Entry(frame_login, textvariable=username, width=30)
         username_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
-        print(username)
 
         # password
         label_password = ttk.Label(frame_login, text='Password:')
@@ -208,21 +182,31 @@ class WelcomeView():
             login_view = LoginView()
             LoginView.showLoginView(login_view, root)
 
-        # Start page
+        # If the user click on the register button
+
+        def register():
+
+            # Destroy frame_welcome
+            frame_welcome.destroy()
+
+            # Create login_frame
+            register_view = RegisterView()
+            RegisterView.showRegisterView(register_view, root)
+
+        # Welcome page
         
         frame_welcome = tk.Frame(root)
 
-        label_welcome = tk.Label(frame_welcome, text='Welcome to Moneytor')
-        label_welcome.grid(column=0, row=0, sticky=tk.EW, padx=5, pady=5)
+        label_welcome = ttk.Label(frame_welcome, text='Welcome to Moneytor')
+        label_welcome.grid(column=0, row=0, sticky=tk.N, padx=5, pady=5)
 
-        label_start_1 = tk.Label(frame_welcome, text='To see your transactions, please log in first')
-        label_start_1.grid(column=0, row=1, sticky=tk.EW, padx=5, pady=5)
+        label_start_1 = ttk.Label(frame_welcome, text='To see your transactions, please log in first')
+        label_start_1.grid(column=0, row=1, sticky=tk.N, padx=5, pady=5)
                 
-        button_login = tk.Button(frame_welcome, text='Log in', command=login)
+        button_login = ttk.Button(frame_welcome, text='Log in', command=login)
         button_login.grid(column=0, row=2, sticky=tk.N, padx=10, pady=10)
 
-        button_signup = tk.Button(frame_welcome, text='Register')
-        button_signup.bind("<Button>", lambda e: RegisterWindow())
+        button_signup = ttk.Button(frame_welcome, text='Register', command=register)
         button_signup.grid(column=0, row=3, sticky=tk.N, padx=10, pady=10)
                 
         frame_welcome.pack()
