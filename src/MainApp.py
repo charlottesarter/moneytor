936cb20277d76edddd8f1db5c8664459636fd318
Max Eberlein <m.eberlein01@gmail.com>
2022-12-09 10:18:36 +0900

    more space for the logo

--- src/MainApp.py
+++ src/MainApp.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     # Create root window of the app
     root = tk.Tk()
-    root.geometry('800x300')
+    root.geometry('800x400')
     root.title('Moneytor')
 
     # Show the first view : the welcome page
